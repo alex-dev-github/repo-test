5db0dfa64caf1e4fc053e3330de0622f75fed047
Alexandr Novikov <alex.dev.workspace@gmail.com>
2021-06-23 19:40:05 +0300

    feature 2

--- Helper/Config.php
+++ Helper/Config.php
@@ -26,6 +26,7 @@ class Config extends AbstractHelper {
     public function isSyncEnabled() : bool {
         $a = 1;
         $a = 2;
+        $b = 1;
 
         return $this->scopeConfig->getValue(
             self::CONFIG_PATH_MODULE_ENABLE,
